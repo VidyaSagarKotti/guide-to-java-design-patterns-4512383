1409de06ba3a2000457147cf7cf5b5049325cf3f
VidyaSagarKotti <162261172+VidyaSagarKotti@users.noreply.github.com>
2025-03-06 01:13:05 +0000

    Factory Method - Creational pattern Example

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,9 +1,26 @@
 package com.example;
 
-public class VendingMachine {
+import java.util.function.Supplier;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.IllegalArgumentException;
 
+public class VendingMachine {
+    private static final Map<String, Supplier<Snack>> snackMap = new HashMap<>();
+
+    static {
+        snackMap.put("Chips", Chips::new);
+        snackMap.put("ChocolateBar", ChocolateBar::new);
+        snackMap.put("Drink", Drink::new);
+    }
+    
     Snack getSnack(String snackType) {
-        return null;
+        if(snackMap.get(snackType) != null){
+            return snackMap.get(snackType).get();
+        }
+        else {
+            throw new IllegalArgumentException("Invalid SnackType");
+        }
     }
 
 
