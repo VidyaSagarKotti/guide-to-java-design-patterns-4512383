af0b9eb243a06ecc0a57c003f50f7067a6961a06
VidyaSagarKotti <162261172+VidyaSagarKotti@users.noreply.github.com>
2025-03-15 17:16:49 +0000

    Memento - Behavioural Pattern Sample

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -3,9 +3,11 @@ package com.example;
 public class Item {
 
     private int price;
+    private ItemState state;
 
     public Item(int price) {
         this.price = price;
+        this.state = new ItemState(price);
     }
 
     public void setPrice(int price) {
@@ -14,6 +16,14 @@ public class Item {
 
     public int getPrice() {
         return price;
+    }
+
+    public void save() {
+        state.setState(price);
+    }
+
+    public void undo() {
+        price = state.getState();
     }
 
 }
